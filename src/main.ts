dbe3c41972608e0e70dc5316bdd12d9699badb70
akash_appscook <akash@appscook.com>
2025-03-09 19:53:52 +0530

    update base hrf

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
 
 import { AppModule } from './app/app.module';
+import { environment } from './app/environment';
 
-
+if (environment.production) {
+  enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const baseHref = environment.baseHref;
+const baseElement = document.createElement('base');
+baseElement.setAttribute('href', baseHref);
+document.head.appendChild(baseElement);
